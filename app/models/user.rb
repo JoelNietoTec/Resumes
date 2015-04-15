71554339d3022020c59fc00a6eba6e42ce831ff8
JoelNietoTec <joelnieto_tec@yahoo.com>
2015-04-15 00:28:21 -0500

    Register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  validates :email, presence: true
+  has_one :profile
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { :with => VALID_EMAIL_REGEX , message: "Formato de email inválido" }
+  validates :email, presence: { message: 'Introducir email' }
+  validates :password, presence: { message: 'Completa este campo' }
+  validates :password, confirmation: { message: 'Contraseñas no coinciden' }
 end
